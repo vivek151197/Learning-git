080acdef9402484034ff1cb6737d4624a5690e78
vivek <vivekbk1997@gmail.com>
2021-11-18 16:26:28 +0530

    Changed the names as per naming convention

---
+++ nodejsapp/namingConvention.js
@@ -0,0 +1,13 @@
+function add(a,b) {
+return a + b;    //spacingBetweenOperators
+}
+//brackets convention : space for opening bracket and closing bracket in next line
+console.log(add(1,2));
+
+var person = {
+firstName: "John",	//camelCasing
+secondName: "Doe"
+};
+//semicolon at the end of object list
+console.log(person.firstName + " " + person.secondName);
+//line length < 80
